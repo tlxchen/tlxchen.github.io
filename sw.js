59e3ecb6fc1b50bb92c5170afd4261b22432bb8b
tlxchen <166957129@qq.com>
2024-11-11 21:00:58 +0800

    Site updated: 2024-11-11 21:00:56

--- sw.js
+++ sw.js
@@ -6,7 +6,7 @@ const preCache = [
   "/css/style.css",
   "/js/script.js",
 ];
-const VERSION = "1731329792870";
+const VERSION = "1731330055324";
 const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
