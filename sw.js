90e19fc27c5bca2cb3a33237f52f55f50f14aa75
tlxchen <166957129@qq.com>
2025-07-26 17:26:08 +0800

    Site updated: 2025-07-26 17:25:59

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = "1753521408305";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
+const VERSION = "1753521958532";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
   "unpkg.com",
